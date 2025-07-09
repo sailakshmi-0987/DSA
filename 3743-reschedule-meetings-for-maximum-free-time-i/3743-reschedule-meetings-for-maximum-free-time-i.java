e82465d44cd9d7575f5cf1c1f5ee94a99dbd447e
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2025-07-09 12:18:02 +0530

    Time: 2 ms (76.11%), Space: 63.7 MB (21.11%) - LeetHub

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        int t =0;
+        int res=0;
+
+        for(int i=0;i<n;i++){
+            t+=endTime[i]-startTime[i];
+            int left = i<=k-1?0:endTime[i-k];
+            int right = i == n-1?eventTime : startTime[i+1];
+            res = Math.max(res,right-left-t);
+
+            if(i>=k-1){
+                t-=endTime[i-k+1]-startTime[i-k+1];
+            }
+        }
+        return res;
+    }
+}
