f8a6b9a59bc8ac7b45faaea8fa8daaee3f3228e3
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2025-07-31 21:37:36 +0530

    Time: 1 ms (6.78%), Space: 40.8 MB (25.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,35 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
+       /* for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 dp[i][j]=-1;
             }
-        }
-        return count(m-1,n-1,dp);
+        }*/
+        dp[0][0] = 1;
+        return count(m,n,dp);
     }
-    public int count(int i,int j,int[][] dp){
-        if(i==0 && j==0){
+    public int count(int m,int n,int[][] dp){
+       /* if(i==0 && j==0){
             return 1;
         }
         if(i<0 || j<0){
             return 0;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];*/
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)dp[0][0]=1;
+                else{
+                    int up=0;
+                    int left = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
 
-        int up = count(i-1,j,dp);
-        int left = count(i,j-1,dp);
-
-        return dp[i][j]=up+left;
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
