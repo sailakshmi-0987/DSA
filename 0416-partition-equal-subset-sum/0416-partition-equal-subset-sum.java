4dc61c74bd7093440c2859abd22320c6d9c989c9
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2025-08-03 15:26:55 +0530

    Time: 44 ms (63.11%), Space: 45.1 MB (70.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -9,10 +9,25 @@ class Solution {
             return false;
         }
         int target = total/2;
-        Boolean[][] dp = new Boolean[n][target+1];
-        return partition(n-1,target,nums,dp);
+        boolean[][] dp = new boolean[n][target+1];
+        for(int i=0;i<n;i++){
+            dp[i][0] = true;
+        }
+        if (nums[0] <= target) dp[0][nums[0]] = true;
+
+        for(int i=1;i<n;i++){
+            for(int k=1;k<=target;k++){
+                boolean notTaken = dp[i-1][k];
+                boolean taken = false;
+                if(nums[i]<=k){
+                    taken = dp[i-1][k-nums[i]];
+                }
+                dp[i][k]=(taken||notTaken);
+            }
+        }
+        return dp[n-1][target];
     }
-    public boolean partition(int index,int target,int[] nums,Boolean[][] dp){
+  /*  public boolean partition(int index,int target,int[] nums,Boolean[][] dp){
         if(index==0){
             return (nums[index]==target);
         }
@@ -30,5 +45,5 @@ class Solution {
         }
 
         return dp[index][target] = (taken || notTaken);
-    }
+    }*/
 }
