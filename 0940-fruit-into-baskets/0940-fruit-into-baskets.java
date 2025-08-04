d99a301f8e28b95206da881d243e668fe126db71
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2025-08-04 14:39:49 +0530

    Time: 56 ms (11.14%), Space: 55.3 MB (40.84%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int start =0;
+        int maxFruits = 0;
+        for(int i=0;i<fruits.length;i++){
+            map.put(fruits[i],map.getOrDefault(fruits[i],0)+1);
+            while(map.size()>2){
+                map.put(fruits[start],map.get(fruits[start])-1);
+                if(map.get(fruits[start])==0){
+                    map.remove(fruits[start]);
+                }
+                start++;
+            }
+            maxFruits = Math.max(maxFruits,i-start+1);
+        }
+        return maxFruits;
+    }
+}
