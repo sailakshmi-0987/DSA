6e32121b72a02f1a89017baa7b7e459488411bee
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2025-08-06 18:17:25 +0530

    Time: 19 ms (86.3%), Space: 51.2 MB (29.76%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -3,25 +3,23 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         if(m==0 || n==0)return 0;
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return count(m-1,n-1,text1,text2,dp);
-    }
-    public int count(int i,int j,String str1,String str2,int[][] dp){
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(str1.charAt(i)==str2.charAt(j)){
-            return dp[i][j]=1+count(i-1,j-1,str1,str2,dp);
-        }
-
-        return dp[i][j]=Math.max(count(i,j-1,str1,str2,dp),count(i-1,j,str1,str2,dp));
+      return dp[m][n];
     }
 }
