f050c158b8cdd42d44d0791938e32188c1443b5b
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2025-08-23 17:40:28 +0530

    Time: 1 ms (99.81%), Space: 42 MB (96.93%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        while(i>=0 || j>=0 || carry==1){
+            if(i>=0){
+                carry+=a.charAt(i--)-'0';
+            }
+            if(j>=0){
+                carry+=b.charAt(j--)-'0';
+            }
+            ans.append(carry%2);
+            carry=carry/2;
+        }
+        return ans.reverse().toString();
+    }
+}
