a18c76152546e5232fb9034a9f352273082aabed
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2026-07-05 19:07:59 +0530

    Time: 449 ms (93.18%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT customer_number 
+SELECT customer_number
 FROM Orders
-GROUP BY customer_number
-ORDER BY count(*) DESC
-LIMIT 1;
+GROUP BY 1
+ORDER BY count(DISTINCT order_number) DESC LIMIT 1;
