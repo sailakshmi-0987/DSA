c3401f1b1daeac75ea8e0cc0adf7e74a0edf57b6
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2026-08-01 20:09:05 +0530

    Time: 0 ms (100%), Space: 43 MB (23.56%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][n];
+        int score = solve(nums,0,n-1,dp);
+        return score>=0;
+    }
+      private int solve(int[] nums, int left, int right, Integer[][] dp) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
+
+        int takeLeft = nums[left] - solve(nums, left + 1, right, dp);
+
+        int takeRight = nums[right] - solve(nums, left, right - 1, dp);
+
+        dp[left][right] = Math.max(takeLeft, takeRight);
+
+        return dp[left][right];
+    }
+}
