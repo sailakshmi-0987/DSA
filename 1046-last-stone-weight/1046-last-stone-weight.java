a3a80933a54b6aec1d57dbaea8472302e1cffb6a
Gajulapalli Sai Lakshmi <gajulapallisailakshmi@gmail.com>
2026-08-16 19:56:40 +0530

    Time: 1 ms (99.7%), Space: 42.9 MB (33.12%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
+        }
+        while(pq.size()>1){
+            int x = pq.poll();
+            int y = pq.poll();
+            
+            if(x!=y){
+                pq.add(x-y);
+            }
+        }
+        if(pq.isEmpty()){
+            return 0;
+        }
+
+        return pq.poll();
+    }
+}
